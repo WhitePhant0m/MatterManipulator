73ed613e337d13f919952257f9220ef1853903a0
WhitePhant0m <kallunki.aleksi@gmail.com>
2026-05-08 15:36:05 +0300

    we aint at 1.0 yet, no way

--- src/main/java/dev/wp/matter_manipulator/MMMod.java
+++ src/main/java/dev/wp/matter_manipulator/MMMod.java
@@ -7,7 +7,6 @@ import dev.wp.matter_manipulator.common.networking.MMNetwork;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
